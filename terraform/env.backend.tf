8b32291b1538c1b45e7024c10faaad167077a48e
Drausin Wulsin <daedalus2718@gmail.com>
2018-01-31 11:53:16 -0500

    move TF backend storage location (#2)

--- terraform/env.backend.tf
+++ terraform/env.backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket      = "elxir-deploy-staging-clusters"
-    prefix      = "primary/terraform"
+    prefix      = "primary-core/terraform"
     project     = "elxir-deply-staging"
     region      = "us-east1"
     credentials = "gcp_credentials.json"
