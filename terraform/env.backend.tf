77ebaad6f47dadf5b38ccd031c25f1dd8d4d615a
Drausin Wulsin <daedalus2718@gmail.com>
2018-03-25 23:52:13 -0400

    use new staging project

--- terraform/env.backend.tf
+++ terraform/env.backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "gcs" {
-    bucket      = "elixir-deploy-staging-clusters"
+    bucket      = "elixir-deploy-staging-1-clusters"
     prefix      = "primary-core/terraform"
-    project     = "elixir-deploy-staging"
+    project     = "elixir-deploy-staging-1"
     region      = "us-east1"
     credentials = "gcp_credentials.json"
   }
