391c0c8290e62a1ba6e819f7c7771f3415fa585b
Drausin Wulsin <daedalus2718@gmail.com>
2018-04-05 21:55:07 -0400

    add Cloud SQL IAM policies

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,7 +29,23 @@ data "google_iam_policy" "deploy-app-admin" {
   }
 
   binding {
+    role = "roles/cloudsql.admin"
+
+    members = [
+      "serviceAccount:terraform-app@elixir-deploy-staging-1.iam.gserviceaccount.com",
+    ]
+  }
+
+  binding {
     role = "roles/datastore.owner"
+
+    members = [
+      "serviceAccount:app-catalog@elixir-deploy-staging-1.iam.gserviceaccount.com",
+    ]
+  }
+
+  binding {
+    role = "roles/cloudsql.client"
 
     members = [
       "serviceAccount:app-catalog@elixir-deploy-staging-1.iam.gserviceaccount.com",
